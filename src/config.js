3b493b6292209dfb5536532eecbcc5659c17e08d
tuqiang01 <tuqiang01@baidu.com>
2019-04-06 22:53:08 +0800

    feat(main)add basic object

---
+++ src/config.js
@@ -0,0 +1,26 @@
+const common = {
+    color:'',
+    bg:0xfffafa,
+    fog:0xf0fff0,
+    camz : 100,
+    camy : 30
+}
+
+const hero = {
+    radius:20
+};
+
+const ground = {
+    radius:100,
+    widSeg:40,
+    heiSeg:40,
+    posy :-100,
+    posz : 10
+
+};
+
+export {
+    common,
+    hero,
+    ground,
+}
--- src/index.js
+++ src/index.js
@@ -6,24 +6,30 @@ let SCREEN_WIDTH = window.innerWidth,
 
 let renderer,camera, scene, controls;
 
+import {hero, ground, common } from "./config";
+
 class Main {
     constructor(){
+
         this.init = () => {
             this.initScene();
             this.initDevTool();
+            this.initObject();
         }
 
         this.initScene = () => {
-            // 创建场景
             scene = new THREE.Scene();
-            // 创建相机
+            // scene.fog = new THREE.FogExp2(common.fog, 0.14 );
             camera = new THREE.PerspectiveCamera(45, SCREEN_WIDTH / SCREEN_HEIGHT, .1, 1000);
-            // 创建渲染器
-            renderer = new THREE.WebGLRenderer();
-            // 添加渲染器至dom
+            camera.position.z = common.camz;
+            camera.position.y = common.camy;
+            renderer = new THREE.WebGLRenderer({alpha:true});
+            renderer.shadowMap.enabled = true;
+            renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+            renderer.setClearColor(common.bg, 1);
             document.body.appendChild(renderer.domElement);
 
-            alert('success! Your browser supports WebGL.')
+            window.scene = scene;
         }
 
         this.initDevTool = () => {
@@ -43,16 +49,51 @@ class Main {
             });
         }
 
-        this.animate = () => {
-            window.requestAnimationFrame(this.animate.bind(this))
-            renderer.render(scene,camera);
+        this.initObject = () => {
+            this.initHero();
+            this.initGroundSphere();
+            this.initLight();
+
+        }
+
+        this.initHero = () => {
+            let geo = new THREE.DodecahedronGeometry( hero.radius, 1);
+            let mat = new THREE.MeshStandardMaterial( { color: 0xe5f2f2 ,shading:THREE.FlatShading} )
+            this.hero = new THREE.Mesh(geo,mat);
+            scene.add(this.hero);
+        }
+
+        this.initGroundSphere = () => {
+            let sphereGeometry = new THREE.SphereGeometry( ground.radius, ground.widSeg,ground.heiSeg);
+            let sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xfffafa ,shading:THREE.FlatShading} )
+            this.rollingGroundSphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+            this.rollingGroundSphere.position.set(0,ground.posy,ground.posz);
+            scene.add(this.rollingGroundSphere);
+        }
+
+        this.initLight = () => {
+            const sun = new THREE.DirectionalLight(0xfffafa,0.5);
+            sun.position.set(0,4,1);
+            sun.castShadow = true;
+            scene.add(sun);
+            sun.shadow.mapSize.width = 256;
+            sun.shadow.mapSize.height = 256;
+            sun.shadow.camera.near = 0.5;
+            sun.shadow.camera.far = 50 ;
+
+            const hemisphereLight = new THREE.HemisphereLight(0xfffafa,0x000000, .9)
+            scene.add(hemisphereLight);
         }
 
         this.init();
+    }
 
-        this.animate();
+    animate(){
+        window.requestAnimationFrame(this.animate.bind(this))
+        renderer.render(scene,camera);
     }
 }
 
 let main = new Main();
+main.animate();
 
